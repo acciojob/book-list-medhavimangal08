c8abb2e5f7cb472b27f25a3ac4e9b716d4a13784
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-19 23:12:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+let btn = document.querySelector("#submit");
+let inputs = document.querySelectorAll("input");
+let tbody = document.querySelector("#book-list");
+
+btn.addEventListener("click", data); // close the parentheses here
+
+function data(e) {
+    e.preventDefault(); // prevent the default form submission behavior
+    let newRow = tbody.insertRow(); // insert a new row
+    for(let i=0; i<inputs.length; i++){
+        let cell = newRow.insertCell(); // insert a new cell
+        cell.innerText = inputs[i].value;
+        inputs[i].value = ''; // clear the input field after adding the data to the table
+    }
+	let clearbtn=document.createElement("button")
+	clearbtn.innerText="X"
+	clearbtn.classList.add("delete")
+	newRow.append(clearbtn)
+	clearbtn.addEventListener("click", ()=>deletedata(newRow))	
+}
+function deletedata(row) {
+	tbody.removeChild(row);
+}
