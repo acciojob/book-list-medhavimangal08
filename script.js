b29d63c7aede148e82ac794cc8d7108b31d42a00
Medhavimangal43 <medhavimangal43@gmail.com>
2024-09-22 18:44:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ let tbody = document.querySelector("#book-list");
 btn.addEventListener("click", data); // close the parentheses here
 
 function data(e) {
-    e.preventDefault();
-    let newRow = tbody.insertRow();
+    e.preventDefault(); // prevent the default form submission behavior
+    let newRow = tbody.insertRow(); // insert a new row
     for(let i=0; i<inputs.length; i++){
-        let cell = newRow.insertCell();
+        let cell = newRow.insertCell(); // insert a new cell
         cell.innerText = inputs[i].value;
-        inputs[i].value = '';
+        inputs[i].value = ''; // clear the input field after adding the data to the table
     }
 	let clearbtn=document.createElement("button")
 	clearbtn.innerText="X"
 	clearbtn.classList.add("delete")
-	cell.append(clearbtn);// append the button to the new cell
+	newRow.append(clearbtn)
 	clearbtn.addEventListener("click", ()=>deletedata(newRow))	
+	function deletedata(row) {
+	tbody.removeChild(row);
 }
+}
+
